2eab02253c5d04962c66b33062cef5942d53bc49
adelito <khalife1001@gmail.com>
2024-10-06 17:44:12 +0300

    boites avec js

--- deal_or_no_deal.js
+++ deal_or_no_deal.js
@@ -6,11 +6,50 @@ const montant_grand = ["$1,000","$5,000","$10,000","$25,000","$50,000","$75,000"
 const montants_malette = [];
 
 function montants() {
+    if (montants_malette.length === 0) { // Only add if array is empty
     for (let i=0;i<26;i++) {
         montants_malette.push(i);
     }
+}
+    return montants_malette;
 }
 
 montants_milieu = montants()
 
 console.log(montants_milieu);
+
+//section gauche
+const leftColumn = document.querySelector(".left-column");
+leftColumn.innerHTML = "";
+
+montant_petit.forEach(valeur =>{
+    const div = document.createElement('div'); // creation nouveau div avec javascript
+    div.classList.add('left_box','box');
+    div.textContent = valeur;
+    leftColumn.appendChild(div);
+}
+);
+
+//section milieu
+const middleColumn = document.querySelector(".middle_column");
+middleColumn.innerHTML = "";
+
+montants_malette.forEach(valeur =>{
+    const div = document.createElement('div');
+    div.textContent = valeur;
+    middleColumn.appendChild(div);
+
+})
+
+//section droite
+const rightColumn = document.querySelector(".right_column");
+rightColumn.innerHTML = "";
+
+montant_grand.forEach(valeur =>{
+    const div = document.createElement('div'); // creation nouveau div avec javascript
+    div.classList.add('right_box','box');
+    div.textContent = valeur;
+    rightColumn.appendChild(div);
+}
+);
+
